a6c21e759cf405b3a376a5dd1828b1b612fe7b3a
clairesay <57375486+clairesay@users.noreply.github.com>
2021-06-01 22:10:43 +1000

    added new pronouns testing some methods of sunburst chart

--- public/js/script.js
+++ public/js/script.js
@@ -177,22 +177,40 @@ Plotly.d3.csv("https://raw.githubusercontent.com/clairesay/DECO3100_A3_csay9246/
 // // Scatter plot demonstrating the topics discussed in the lead up to election y-axis: sentiment vertically, date horizontally
 Plotly.d3.csv("https://raw.githubusercontent.com/clairesay/DECO3100_A3_csay9246/main/public/data/child_values.csv", function(err, rows){
 console.log(["First person", "Second person", "Third person", "Collective"].concat(unpack(rows, 'words')))
-var numbers = [25, 25, 25, 25].concat(unpack(rows, 'trump_all_value'))
-// console.log([25, 25, 25, 25].concat(unpack(rows, 'trump_all')),)
+var percentages = unpack(rows, 'trump_all_value')
+// console.log(percentages)
+percentages = percentages.map(function(x) {
+    let whole;
+    if (percentages.indexOf(x) <= 3 ) {
+        whole = 21
+    } else if (percentages.indexOf(x) <= 7) {
+        whole = 12
+    } else if (percentages.indexOf(x) <= 18) {
+        whole = 43
+    } else if (percentages.indexOf(x) <= 21) {
+        whole = 24
+    }
+    return parseInt(x, 10)/100 * whole
+})
+// 21, 12, 43, 24
+//               [21, 12, 43, 24, 12.18, 2.31, 6.09, 0, 5.16, 0.72, 6, 0, 6.0200000000000005, 1.29, 9.46, 8.6, 1.29, 0.86, 1.72, 3.44, 0, 3.8699999999999997, 3.8699999999999997, 17.52, 6.24, 0]
+percentages = [21, 12, 43, 24].concat(percentages) //
+// var numbers = [21, 12, 43, 24].concat(unpack(rows, 'trump_all_value'))
+// // console.log([25, 25, 25, 25].concat(unpack(rows, 'trump_all')),)
 
-var result = numbers.map(function (x) { 
-  return parseInt(x, 10); 
-});
-console.log(result)
+// var result = numbers.map(function (x) { 
+//   return parseInt(x, 10); 
+// });
+// console.log(result)
 // .concat(unpack(rows, 'words'))
 var data = [
         {
           "type": "sunburst",
-          "labels": ["First person", "Second person", "Third person", "Collective"].concat(unpack(rows, 'words')), 
+          "labels": ["First person", "Collective", "Third person", "Second person"].concat(unpack(rows, 'words')), 
           "parents": ["", "", "", ""].concat(unpack(rows, 'parent')),
         //   who is their parent: ["", "Personal", "Personal", "Personal", "Personal, "]
         //   "values":  [25, 25, 25, 25].concat(unpack(rows, 'trump_all')),
-            "values": result,
+            "values": percentages,
           "leaf": {"opacity": 0.4},
           "marker": {"line": {"width": 2}},
           "branchvalues": 'total'
